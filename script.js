c905a7b19162c2357b88ad38e6919c419df0812e
banjofrogg <168992376+banjofrogg@users.noreply.github.com>
2024-11-10 20:19:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let diagrams = document.getElementsByClassName("diagrams")[0];
 
 function toggleDiagrams() {
-  if(diagrams.style.display == "none") diagrams.style.display = "flex";
-  else diagrams.style.display = "none";
+  if(diagrams.style.display == "flex") diagrams.style.display = "none";
+  else diagrams.style.display = "flex";
 }
 
 document.getElementsByClassName("close")[0].addEventListener("click", toggleDiagrams);
